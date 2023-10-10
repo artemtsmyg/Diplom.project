152245560b0b13280d70e87418cbd33b4c63b984
Артём <artemtsmyg@yandex.ru>
2023-10-10 15:33:32 +0300

    Разработка моб.приложений2.

---
+++ Содержание дипломного проекта/4.Список использованной литературы и ресурсов/1)Список использованной литературы и ресурсов.py
@@ -0,0 +1,61 @@
+#                Список использованной литературы и ресурсов.
+
+#      Вот список возможных источников литературы и ресурсов, которые можно 
+# 
+# использовать для исследования мобильных приложений:
+
+#                           1. Книги:
+
+# - "Designing Mobile Interfaces" автора Steven Hoober и Eric Berkman
+# - "Mobile Design Pattern Gallery: UI Patterns for Mobile Applications"
+ 
+# автора Theresa Neil
+
+# - "Mobile Usability" автора Jakob Nielsen
+
+# - "App Design Handbook" автора Nathan Barry
+
+# - "The Mobile Application Hacker's Handbook" авторы Dominic Chell,
+ 
+# Tyrone Erasmus, Shaun Colley и Ollie Whitehouse
+
+#                   2. Научные статьи и публикации:
+
+# - Блоги и публикации на сайтах сервисов разработки приложений, 
+
+# таких как Apple Developer, Google Developers и Microsoft Developer Network
+
+# - Исследования и отчеты от учреждений, занимающихся мобильным рынком, 
+
+# например, App Annie и Sensor Tower
+# - Журналы и публикации, посвященные мобильной разработке, например, 
+
+# "Mobile App Development & Testing" и "Mobile App Development Magazine"
+
+#                    3. Курсы и учебные ресурсы:
+
+# - Онлайн-курсы и платформы онлайн-обучения, такие как Coursera, 
+
+# Udemy и edX, предлагают курсы по разработке мобильных приложений
+
+# - Руководства и документация от ведущих платформ разработки мобильных приложений,
+ 
+# например, Android Developers Guide и Apple Developer Documentation
+
+#                 4. Отзывы и опыт от пользователей:
+
+# - Форумы разработчиков и сообщества мобильных приложений, такие как 
+
+# Stack Overflow и Reddit
+
+# - Обзоры приложений в магазинах приложений, таких как Google Play Store и 
+
+# Apple App Store
+
+# - Интервью с пользователями и проведение пользовательских тестов
+
+# Помните, что каждое исследование может требовать индивидуального 
+
+# подхода к выбору источников, и важно обеспечить авторитетность и достоверность 
+
+# информации, используемой в вашем исследовании.
